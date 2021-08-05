8997a4644158da63303356318028977b19d97608
Alex McCaffrey <alex.mccaffrey07@gmail.com>
2021-08-05 08:22:21 -0600

    cleaned up unused code

--- src/Chat/Chat.js
+++ src/Chat/Chat.js
@@ -8,16 +8,8 @@ function Chat() {
   var socket = io();
 
   var messages = document.getElementById("messages");
-  //var form = document.getElementById("form");
   var input = document.getElementById("input");
 
-  //   form.addEventListener("submit", function (e) {
-  //     e.preventDefault();
-  //     if (input.value) {
-  //       socket.emit("chat message", input.value);
-  //       input.value = "";
-  //     }
-  //   });
 
   const handleChange = (e) => {
     setInputText(e.target.value);
@@ -25,7 +17,6 @@ function Chat() {
 
   const updateMessages = (msg) => {
     var item = document.createElement("li");
-      console.log("inside socket")
       item.textContent = msg;
       messages.appendChild(item);
       window.scrollTo(0, document.body.scrollHeight);
